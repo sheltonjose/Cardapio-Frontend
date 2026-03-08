6925a47196494b91d29c6a0e9f2c8f86296535ef
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-03-08 21:07:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import type { FoodData } from './interface/FoodData';
 
 function App() {
-
+  const data: FoodData =[];  // dados que vem da API
 
   return (
     <div className="container">
       <h1>Cardápio</h1>
         <div className="card-grid">
-
+          {data.map(foodData=><Card price={foodData.price} title={foodData.title} image={foodData.image}/>)}
         </div>
 
 
