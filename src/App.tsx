3a6346b9a35f9f96af1c94df89ec13dd304ed03b
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-03-12 22:04:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import type { FoodData } from './interface/FoodData';
+import { Card } from './components/card/card';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  const data: FoodData =[];  // dados que vem da API
-
+  //const data: FoodData[] =[];  // dados que vem da API
+   const {data}= useFoodData();    // dados que vem da API
   return (
     <div className="container">
       <h1>Cardápio</h1>
         <div className="card-grid">
-          {data.map(foodData=><Card price={foodData.price} title={foodData.title} image={foodData.image}/>)}
+          {data.map(foodData=>
+          <Card 
+            price={foodData.price} 
+            title={foodData.title}
+            image={foodData.image}
+            />
+            )}
         </div>
 
 
